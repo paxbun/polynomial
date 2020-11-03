d485026c8d5cfd7defdafd91830be5f99a610770
Chanjung Kim <freiyer.paxbun@gmail.com>
2020-11-04 02:00:11 +0900

    Update example

--- polynomial-test.cc
+++ polynomial-test.cc
@@ -10,17 +10,31 @@ int main()
 {
     while (true)
     {
-        cout << "p = ";
-        string str;
-        getline(cin, str);
-        if (str.empty()) break;
-
         try
         {
-            auto p = Polynomial<double>::FromString(str);
+            cout << "p = ";
+            string pStr;
+            getline(cin, pStr);
+            if (pStr.empty()) break;
+            auto p = Polynomial<double>::FromString(pStr);
+
+            cout << "q = ";
+            string qStr;
+            getline(cin, qStr);
+            if (qStr.empty()) break;
+            auto q = Polynomial<double>::FromString(qStr);
+
             cout << "p = " << p << endl;
+            cout << "q = " << q << endl;
+            cout << "p + q = " << p + q << endl;
+            cout << "p - q = " << p - q << endl;
+            cout << "p * q = " << p * q << endl;
+            cout << "2p = " << 2 * p << endl;
+            cout << "2q = " << 2 * q << endl;
             cout << u8"p² = " << pow(p, 2) << endl;
             cout << u8"p³ = " << pow(p, 3) << endl;
+            cout << u8"q² = " << pow(q, 2) << endl;
+            cout << u8"q³ = " << pow(q, 3) << endl;
         }
         catch (std::invalid_argument)
         {
